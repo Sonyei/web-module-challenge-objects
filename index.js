8a27e50ed8ea09b06c02c6f7e360e6aac68be092
Angelika Blackburn <bma89contact@gmail.com>
2020-09-10 12:04:29 -0400

    broken, but doing something

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ function createMenuItem(Name, Cost, Category){
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
 
+console.log(newItem = new createMenuItem("Fajitas", 10, "Dinner"));
+console.log(newItem = new createMenuItem("Fideos con Pollo", 10, "Lunch"));
+console.log(newItem = new createMenuItem("Tamales", 10, "Dinner"));
 
 
 /* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
@@ -30,6 +33,19 @@ and should return a number.
 
 For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
 
+burger.discount = function Discount(occupation) {
+  if(occupation = "teacher" || "student"){
+    console.log(this.price * 0.25);
+  } else if(occupation = "public"){
+    console.log(this.price * 0.10);
+    }
+    else {
+      console.log(ERR);
+  }
+};
+
+
+console.log(burger.discount("teacher"));
 
 
 ///////////////Reviews (MVP)///////////////////
